c91e91ded9561551a79101b21e8c1a9cc10bf181
Jongie Oaquiera <joaquiera1@nait.ca>
2026-05-28 16:59:49 -0600

    added a method for players to play a card

--- card_game_v2/player.py
+++ card_game_v2/player.py
@@ -1,3 +1,5 @@
+import random
+
 class Player:
     # needs name, whether player is bot, their card on hand, penalty points
     def __init__(self, name, is_bot):
@@ -6,6 +8,22 @@ class Player:
         self.hand = []
         self.penalty = 0
 
+    def play_card(self):
+        card_played = None
+        if self.is_bot:
+            # pick random card on hand
+            card_played = random.choice(self.hand)
+
+        else:
+            # if human player, display current hand
+            print("your hand")
+            print(self.hand)
+            card_played = int(input("Pick card to play --> "))
+        
+        # remove card from hand
+        self.hand.remove(card_played)
+        # display card played
+        print(f"{self.name} played -> {card_played}")
 
     def __str__(self):
         return f"{self.name} joins the game"
