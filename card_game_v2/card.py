5f17854470deb0c9b535d8f4c8fd72ebec2a50e6
Jongie Oaquiera <joaquiera1@nait.ca>
2026-05-28 17:35:59 -0600

    created card class

---
+++ card_game_v2/card.py
@@ -0,0 +1,17 @@
+import random
+
+class Card:
+    # card needs value, penalty value.
+    def __init__(self, value):
+        self.value = value
+        # for now, have a random value for penalties
+        self.penalty = random.randint(1,10)
+
+    def __str__(self):
+        return f"Card# {self.value} (PP{self.penalty})"
+    
+    def __eq__(self, other):
+        if isinstance(other, Card):
+            return self.value == other.value
+        else:
+            return False
