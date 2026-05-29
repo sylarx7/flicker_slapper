2c302046fc5236ca90595ccd1826f6d5acc2f392
Jongie Oaquiera <joaquiera1@nait.ca>
2026-05-29 16:16:54 -0600

    added note for the current version of this game

--- card_game_v2/__main__.py
+++ card_game_v2/__main__.py
@@ -2,3 +2,10 @@ from .game import Game
 if __name__ == "__main__":
     g = Game()
     g.PlayGame()
+
+
+# NOTE
+# this version doesnt not include input validation for user meaning
+#   that it will crash if user played a card that is not present
+#   in their current hand.
+# Reason is that this will be ported to unity anyway where the game will be polished more.
