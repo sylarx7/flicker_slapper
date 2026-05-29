6ce8c7d29f8f87f051c44185c0c64b7d92a4f755
Jongie Oaquiera <joaquiera1@nait.ca>
2026-05-28 18:22:23 -0600

    created build and shuffle methods

--- card_game_v2/utilities/deck.py
+++ card_game_v2/utilities/deck.py
@@ -7,3 +7,19 @@ class Deck:
     
     def __str__(self):
         return f"{self.deck}"
+    
+    #  METHODS
+    # create a deck with 100 cards
+    def build(self):
+        # starting value
+        value = 1
+        while len(self.deck) != 100:
+            # create card with increasing value
+            card = Card(value)
+            self.deck.append(card)
+            value += 1
+
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+
